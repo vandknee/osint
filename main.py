67ced0afcacb54d54c01ed9601cd7d1acbc280ea
whocodeswhat <mehtamishthi2@gmail.com>
2026-06-30 01:08:18 +0530

    changing stuff

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from modules.username import (
-    check_github,
-    check_reddit,
-    check_gitlab,
-    check_hackerone
-)
+from modules.github import check_github
 
 username = input("Enter username: ")
 
-print(f"GitHub:    {check_github(username)}")
-print(f"Reddit:    {check_reddit(username)}")
-print(f"GitLab:    {check_gitlab(username)}")
-print(f"HackerOne: {check_hackerone(username)}")
+print(check_github(username))
--- modules/username.py
+++ modules/username.py
@@ -1,58 +1,19 @@
-import requests
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0"
-}
-
+from playwright.sync_api import sync_playwright
 
 def check_github(username):
     url = f"https://github.com/{username}"
 
-    response = requests.get(url, headers=HEADERS)
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
 
-    if response.status_code == 200:
-        return "FOUND"
-    elif response.status_code == 404:
-        return "NOT FOUND"
-    else:
-        return f"ERROR ({response.status_code})"
+        page.goto(url)
 
+        if page.title() == "Page not found · GitHub · GitHub":
+            result = "NOT FOUND"
+        else:
+            result = "FOUND"
 
-def check_reddit(username):
-    url = f"https://www.reddit.com/user/{username}"
+        browser.close()
 
-    response = requests.get(url, headers=HEADERS)
-
-    # Reddit often returns 200 even for missing users
-    if "Sorry, nobody on Reddit goes by that name." in response.text:
-        return "NOT FOUND"
-    elif response.status_code == 200:
-        return "FOUND"
-    else:
-        return f"ERROR ({response.status_code})"
-
-
-def check_gitlab(username):
-    url = f"https://gitlab.com/{username}"
-
-    response = requests.get(url, headers=HEADERS)
-
-    if response.status_code == 200:
-        return "FOUND"
-    elif response.status_code == 404:
-        return "NOT FOUND"
-    else:
-        return f"ERROR ({response.status_code})"
-
-
-def check_hackerone(username):
-    url = f"https://hackerone.com/{username}"
-
-    response = requests.get(url, headers=HEADERS)
-
-    if response.status_code == 200:
-        return "FOUND"
-    elif response.status_code == 404:
-        return "NOT FOUND"
-    else:
-        return f"ERROR ({response.status_code})"
+    return result
